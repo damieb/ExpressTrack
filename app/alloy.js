3751dc0a491c33c6a0572aa946c6ecdcc603fc69
Yanis Adoui <contact@devhacker.io>
2014-11-03 22:37:27 +0100

    [FIX] Problem with global declaration

--- app/alloy.js
+++ app/alloy.js
@@ -8,7 +8,7 @@
 //# Include common libraries to the application 
 Alloy.Globals.libs = {};
 Alloy.Globals.libs.helper = require('helper');
-Alloy.Globals.libs.transporters = require('transporters').client();
+Alloy.Globals.libs.transporters = require('transporters');
 
 //# Load widget for loader
 Alloy.Globals.loading = Alloy.createWidget('nl.fokkezb.loading');
